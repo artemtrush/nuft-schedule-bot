1a7dac1ba44498c994e43993f03a87ec4fa7e506
atrush <atrush.main@gmail.com>
2018-09-30 01:41:05 +0300

    Create from template

--- public/index.php
+++ public/index.php
@@ -1,14 +1,11 @@
 <?php
 
-echo "TEST";
-
-/*
 ini_set('default_socket_timeout', 600);
 date_default_timezone_set('Europe/Kiev');
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $appConf = include_once __DIR__ . '/../etc/app-conf.php';
 
-Engine\Engine::setConnection('db', $appConf['dbConfig']);
+// Engine\Engine::setConnection('db', $appConf['dbConfig']);
 $app = AppFactory::create($appConf);
 $app->run();
