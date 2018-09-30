f2cac7ee5a3e5e086c75a9ec082f7951a81b266e
atrush <atrush.main@gmail.com>
2018-09-30 21:34:09 +0300

    Add Service Schedule Show

--- lib/Controller/Help.php
+++ lib/Controller/Help.php
@@ -6,13 +6,16 @@ class Help extends Base
 {
     public function run($message)
     {
+
+        $thunderstorm = u'\U0001F4A8';
+
         $text =
             '/start - текст' . PHP_EOL .
             '/help  - текст' . PHP_EOL .
             '/today - текст' . PHP_EOL .
             '/start - текст' . PHP_EOL .
             '/start - текст' . PHP_EOL .
-            '/start - \U+0037'
+            '/start - ' . $thunderstorm
         ;
 
         return $text;
